af87954ada656e48f54c872a4e7257002587ebb4
Elis Odenhage <teknikgillaren@gmail.com>
2022-10-03 13:31:48 +0200

    list projects

---
+++ js/listProjects.js
@@ -0,0 +1,30 @@
+const table = document.getElementById("table")
+
+const list = (data) => {
+    for(let i = 0; i < data.length; i++){
+	const row = document.createElement("tr");
+
+	const col1 =document.createElement("th")
+	const col2 =document.createElement("th")
+	const col3 =document.createElement("th")
+	const col4 =document.createElement("th")
+
+	col1.innerText = data[i].title
+	col2.innerText = data[i].description
+	col3.innerText = data[i].author
+	col4.innerText = data[i].repo
+
+	row.appendChild(col1)
+	row.appendChild(col2)
+	row.appendChild(col3)
+	row.appendChild(col4)
+	      
+	table.appendChild(row)
+    }
+}
+
+axios.get("http://127.0.0.1:3001/projects")
+    .then((res) => {
+	console.log(res);
+	list(res.data)
+    });
