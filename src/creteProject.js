1b08055ca3e4344ad402ef24d4eafde2e131390b
Elis Odenhage <teknikgillaren@gmail.com>
2022-10-17 11:28:29 +0200

    create project

---
+++ src/creteProject.js
@@ -0,0 +1,68 @@
+import React from 'react'
+
+function CreateProject(){
+    const titleRef = useRef(null);
+    const descRef = useRef(null);
+    const repoRef = useRef(null);
+    const authorRef = useRef(null);
+
+    const success = () => {
+        location.href = "loggedIn";
+        console.log("your in");
+    };
+
+    const failed = () => {
+        console.log("Try again");
+    };
+
+    const resetToken = () => {
+        axios.post("http://127.0.0.1:4001/token", {
+	    "token": localStorage.getItem("refreshToken")
+        }).then((res) => {
+	    localStorage.setItem("accessToken", res.data.accessToken);
+	    create();
+        });
+    };
+
+    const create = () => {
+        axios.post("http://127.0.0.1:3001/createProject", {
+	    title: titleRef.current.value,
+	    desc: descRef.current.value,
+	    repo: repoRef.current.value,
+	    author: authorRef.current.value
+        }, {
+	    headers: {
+	        'authorization': 'Bearer ' + localStorage.getItem("accessToken")
+	    }
+        })
+	    .then((res) => {
+	        console.log(res);
+	        if(res.data == "Success"){
+		    success();
+	        }else{
+		    failed();
+	        }
+	    })
+	    .catch((err) => {
+	        if(err.response.status == 403){
+		    resetToken();
+	        }
+	    });
+    };
+
+    return (
+        <div className="wrapper">
+          <label>Title:</label>
+          <input type="text" id="title" ref={titleRef}/>
+          <label>Description:</label>
+          <input type="text" id="desc" ref={descRef}/>
+          <label>Autor:</label>
+          <input type="text" id="author" ref={authorRef}/>
+          <label>Github Repo:</label>
+          <input type="text" id="repo" ref={repoRef}/>
+          <button onClick={create}>Create</button>
+        </div>
+    );
+}
+
+export default CreateProject;
